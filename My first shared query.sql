633f2ef02dc2f9afd34ce81b32fb7d8338e204f2
Ian Wakoli <wakoliian2007@gmail.com>
2026-02-11 16:01:37 +0300

    Add files via upload

---
+++ My first shared query.sql
@@ -0,0 +1,34 @@
+DROP TABLE student;
+CREATE TABLE student(
+    student_Id  INT  PRIMARY KEY,
+    name  VARCHAR(20) NOT NULL,
+    major VARCHAR(20) UNIQUE,
+);
+DESCRIBE student;
+ALTER TABLE student ADD GPA DECIMAL(3,2);
+DROP TABLE student;
+ALTER TABLE student ADD school VARCHAR(20);
+ALTER TABLE student DROP gpa;
+SELECT * FROM student;
+INSERT INTO student VALUES(2, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
+INSERT INTO student(student_Id, name, major, GPA) VALUES(5, 'Ian', 'Cyber security', 4.5);
+INSERT INTO student(student_Id, name, major, school) VALUES(6, 'Ian', 'Cyber security', 'kirinyaga');
+INSERT INTO student VALUES(9, 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
+
+DROP TABLE Students;
+
+CREATE TABLE Students(
+    student_Id  INT,
+    name  VARCHAR(20) NOT NULL,
+    major VARCHAR(20) UNIQUE,
+    PRIMARY KEY(student_Id)
+);
+
+ALTER TABLE Students ADD school VARCHAR(20);
+ALTER TABLE Students ADD GPA DECIMAL(3,2);
+
+SELECT * FROM Students;
+INSERT INTO Students VALUES(2, 'Professor Smalla', 'Computer science', 'Kirinyaga', 4.6 );
+INSERT INTO Students(student_Id, name, major, GPA) VALUES(5, 'Ian', 'Cyber Technician', 4.5);
+INSERT INTO Students(student_Id, name, major, school) VALUES(6, 'Ian', 'Cyber security', 'kirinyaga');
+INSERT INTO Students VALUES(9, 'Professor Smalla', 'Computer Engineering', 'Kirinyaga', 7.6 );
